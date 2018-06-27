ee27b9f798964d6465b6819bbb78d1cdc5889bab
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-06-28 01:26:35 +0300

    Update read.py

--- read.py
+++ read.py
@@ -3,11 +3,21 @@
 
 import urllib.request
 
-def source(url="http://www.haberturk.com/rss/spor.xml"):
-    s=urllib.request.urlopen(url)
-    return s.read().splitlines()
+class rss(object):
+    def source(self, url="http://www.haberturk.com/rss/spor.xml"):
+        s=urllib.request.urlopen(url)
+        return s.read().splitlines()
+    def read(self):
+        news = {}
+        for j in self.source():
+            j = j.decode('utf-8', 'backslashreplace')
+            if j.find("<title>") > -1 and j.find("CDATA") > -1:
+                j = j.replace("[","")
+                j = j[j.find("!CDATA")+len("!CDATA"):].split("]]")[0]
+                news.update({"title":j})
+        return news
 
-for j in source():
-    j = j.decode('utf-8', 'backslashreplace')
-    if j.find("<title>") > -1 and j.find("CDATA") > -1:
-        print (j)
+
+beta = rss()
+
+print (beta.read())
