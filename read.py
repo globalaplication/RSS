4be7cf2e97c325858b0a256602795370eb5144fd
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-06-28 01:41:51 +0300

    Update read.py

--- read.py
+++ read.py
@@ -7,17 +7,22 @@ class rss(object):
     def source(self, url="http://www.haberturk.com/rss/spor.xml"):
         s=urllib.request.urlopen(url)
         return s.read().splitlines()
-    def read(self):
+    def read(self, tag="<title>"):
         news = {}
         for j in self.source():
             j = j.decode('utf-8', 'backslashreplace')
-            if j.find("<title>") > -1 and j.find("CDATA") > -1:
+            if j.find(tag) > -1 and j.find("CDATA") > -1:
                 j = j.replace("[","")
                 j = j[j.find("!CDATA")+len("!CDATA"):].split("]]")[0]
-                news.update({"title":j})
+                news.update( [ {j:""} ])
         return news
+
+    def get(self):
+        tags = ["<title>", "<description>", "<pubDate>"]
+        for j in tags:
+            print (j)
 
 
 beta = rss()
 
-print (beta.read())
+print (beta.read("<pubDate>"))
